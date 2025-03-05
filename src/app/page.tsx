ee52e0cb0cc47977797403fec2e4b7ef06a5b6de
Tharinda Anurajeewa <tharindahp@gmail.com>
2025-03-05 11:34:20 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className='w-full bg-yellow-200'>
+      <div className='container bg-green-300'>
+        <Button className='bg-red-900'>Hello</Button>
+      </div>
+    </div>
+  );
 }
